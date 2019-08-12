75ec2565c0c086151c18e169ee6c747e8f6ee821
Isai Bismark Cortez <isai0505@gmail.com>
2019-08-12 00:11:20 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,14 @@
 class Person
-  def initialize(name, job)
-    @name = name
-    @job = job
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+  def name
+    @name
+  end
+  def job=(person_job)
+    @job = person_job
+  end
+  def job
+    @job
   end
 end
