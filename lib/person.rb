7cd163237e38c1736bb045b11d5182ceac762677
Isai Bismark Cortez <isai0505@gmail.com>
2019-08-12 00:02:27 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,4 +3,9 @@ class Person
     @name = name
     @job = job
   end
+  def name=(full_name)
+    first_name, last_name = full_name.split
+    @first_name = first_name
+    @last_name = last_name
+  end
 end
